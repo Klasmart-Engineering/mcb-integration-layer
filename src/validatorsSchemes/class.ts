14258d510045c69a83f4d9341216446386638acc
Ionut.Rusu <ionut.rusu@endava.com>
2021-12-30 10:07:52 +0000

    Merged in feature/CIL-50-class-validation-contract (pull request #23)
    feat (CIL-50): class validation contract
    
    * feat: class validation contract
    
    * fix: update shortCode max lenght
    
    
    Approved-by: Victor.Cordos
    Approved-by: Cang Ta

---
+++ src/validatorsSchemes/class.ts
@@ -0,0 +1,52 @@
+import Joi from 'joi';
+import { validationRules } from '../config/validationRules';
+
+export const classSchema = Joi.object({
+    name: Joi.string()
+        .min(validationRules.CLASS_NAME_MIN_LENGTH)
+        .max(validationRules.CLASS_NAME_MAX_LENGTH)
+        .required(),
+
+    clientUuid: Joi.string()
+        .guid()
+        .required(),
+    
+    klOrgUuid: Joi.string()
+        .guid()
+        .required(),
+
+    shortCode: Joi.string()
+        .max(validationRules.CLASS_SHORT_CODE_MAX_LENGTH),
+
+    schoolName: Joi.string()
+        .max(validationRules.SCHOOL_NAME_MIN_LENGTH)
+        .max(validationRules.SCHOOL_NAME_MAX_LENGTH)
+        .required(),
+
+    clientOrgUuid: Joi.string()
+        .guid()
+        .required(),
+
+    programNames: Joi.array()
+        .items(
+            Joi.string()
+            .min(validationRules.PROGRAM_NAME_MIN_LENGTH)
+            .max(validationRules.PROGRAM_NAME_MAX_LENGTH)
+        )
+        .required(),
+
+    client: Joi.string()
+        .max(validationRules.CLIENT_MAX_LENGTH),
+
+    errors: Joi.array()
+        .items(Joi.string())
+        .required(),
+
+    status: Joi.string()
+        .max(validationRules.STATUS_MAX_LENGTH),
+    
+    organizationName: Joi.string()
+        .min(validationRules.ORGANIZATION_NAME_MIN_LENGTH)
+        .max(validationRules.ORGANIZATION_NAME_MAX_LENGTH)
+        .required(),
+});
